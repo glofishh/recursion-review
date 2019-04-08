135cba8e390263852317554535eeac350a30c5ae
glofishh <gloriaulee@gmail.com>
2019-04-08 14:55:25 -0400

    completed JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,46 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //array container
+  var outputArr = [];
+
+  //if null, boolean, number, string
+  if (obj === null) {
+    return 'null';
+  }
+  if (obj === 'boolean') {
+    return '' + obj;
+  }
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  }
+
+  //if object is an array, iterate
+  if(Array.isArray(obj)){
+    if(obj[0] === undefined){
+      return '[]';
+    } else{
+      obj.forEach(function(value) {
+        outputArr.push(stringifyJSON(value))
+      });
+    } return '[' + outputArr + ']';
+  }
+
+  //if object
+  if (obj instanceof Object) {
+    var outputArr2 = [];
+    if(Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    for (var key in obj) {
+      if(obj[key] === undefined) {
+        return '{}';
+      }
+      outputArr2.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+  return '{' + outputArr2 + '}';
+  }
 };
