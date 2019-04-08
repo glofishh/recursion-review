59418da30a81b1b1fe93dd68c06d263e9fb36e2b
glofishh <gloriaulee@gmail.com>
2019-04-08 19:09:38 -0400

    completed getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,15 +9,15 @@ var getElementsByClassName = function(className) {
   // output container
   var outputArr = [];
 
-  var nodesExists = function(node) {
-    if (node.classList && node.classList.contains(className)){
-      outputArr.push(node);
-    } if(node.hasChildNodes()){
-      for(var i = 0; i < node.childNodes.length; i++){
-        nodesExists(node.childNodes[i]);
+  var nodesExists = function(element) {
+    if (element.classList && element.classList.contains(className)){
+      outputArr.push(element);
+    } if(element.hasChildNodes()){
+      for(var i = 0; i < element.childNodes.length; i++){
+        nodesExists(element.childNodes[i]);
       }
     }
   }
-  nodesExists(document); //getting inner function to run -- invoking
+  nodesExists(document.body); //getting inner function to run -- invoking
   return outputArr;
 };
